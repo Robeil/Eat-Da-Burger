b7722ce1e12b1dad70652d401bf105a11f7a8967
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-09-10 12:25:14 -0700

    created data base

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name STRING_AGG NOT NULL,
+	devoured BOOLEAN DEFAULT false,   -- false or true got ask
+	PRIMARY KEY (id)
+);
