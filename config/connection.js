7abfdb0a010dae21d462611a26a9b2354e9216e8
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-09-16 20:00:37 -0700

    adjusting jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,11 @@
 var mysql = require("mysql");
 const util = require('util');
 
+//JawsDB
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
 var connection = mysql.createConnection({
   host: "	nwhazdrp7hdpd4a4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
@@ -10,7 +15,7 @@ var connection = mysql.createConnection({
   password: "hc5mudnhz9xehqg3",
   database: "	bl7noh8gkl8dt0xo"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
